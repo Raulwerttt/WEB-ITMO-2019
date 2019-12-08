134a1d1ccf45ad94695d38508a30b6b8b8e3020d
Raulwerttt <44097987+Raulwerttt@users.noreply.github.com>
2019-12-08 21:47:05 +0300

    Update js.js

--- JS/Tables/js.js
+++ JS/Tables/js.js
@@ -1,125 +1,76 @@
-function saveText(row, column){
-	let text = document.getElementsByTagName("table")[0].children[0]
-		.children[row].children[column].children[0].value;
-	document.getElementsByTagName("table")[0].remove();
-	document.write(text);
-}
-
-function changeWidth(){
-	let width = document.getElementsByTagName("div")[0].children[0].value;
-	let border = document.getElementsByTagName("div")[0].children[1].value;
-	document.getElementsByTagName("table")[0].style = "width: " +  width
-		+ "px; border: 2px " + border + ' black;';
-}
-function changeWidthText(){
-	let width = document.getElementsByTagName("div")[0].children[0].value;
-	let border = document.getElementsByTagName("div")[0].children[1].value;
-	document.getElementsByTagName("div")[0].children[2].value = "Submit with width " + width
-		+ "px and " + border + " border";
-}
-
-function addHeader(){
-	let header = document.getElementsByTagName("div")[1].children[0].value
-	document.getElementsByTagName("tbody")[0].innerHTML = "<tr><th colspan='"
-		+ document.getElementsByTagName("tbody")[0].children[1].children.length + "'>" + header + "</th></tr>"
-		+ document.getElementsByTagName("tbody")[0].innerHTML;
-}
-
-function deleteRow(){
-	let row = document.getElementsByTagName("div")[2].children[0].value;
-	let tableRows = document.getElementsByTagName("tbody")[0].children.length;
-	try {
-		document.getElementsByTagName("tbody")[0].children[row-1].remove();
-	}
-	catch {
-		alert("400 Bad Request");
-	}
-}
-
-function magic(){
-	let row = Math.floor(Math.random()*document.getElementsByTagName("table")[0]
-		.children[0].children.length);
-	let column = Math.floor(Math.random()*document.getElementsByTagName("table")[0]
-		.children[0].children[row].children.length);
-	let cellText = document.getElementsByTagName("table")[0].children[0]
-	.children[row].children[column];
-	let color1 = Math.floor(Math.random() * 256);
-	let color2 = Math.floor(Math.random() * 256);
-	let color3 = Math.floor(Math.random() * 256);
-	switch (Math.ceil(Math.random() * 4)){
-		case 1:
-			cellText.style = "color: rgb(" + color1 + ", " + color2 + ", " + color3 + ")";
-			for (var i = 0; i < cellText.children.length; i++)
-				cellText.children[i].style = "color: rgb(" + color1 + ", " + color2
-					+ ", " + color3 + ")";
-			break;
-		case 2:
-			cellText.style = "background-color: rgb(" + color1 + ", " + color2
-				+ ", " +color3 + ")";
-			for (var i = 0; i < cellText.children.length; i++)
-				cellText.children[i].style = "background-color: rgb(" + color1 + ", " + color2
-					+ ", " + color3 + ")";
-			break;
-		case 3:
-			cellText.style = 'font-size: ' + Math.ceil(Math.random() * 11 + 14)+ 'pt;';
-			for (var i = 0; i < cellText.children.length; i++)
-				cellText.children[i].style = 'font-size: ' + Math.ceil(Math.random() * 11 + 14)
-			+ 'pt;';
-			break;
-		case 4:
-			cellText.remove();
-			break;
-	}
-}
-
-function deleteThis(){
-	for (var i = 0; i < document.getElementsByTagName("body")[0].children.length; i++)
-		document.getElementsByTagName("body")[0].children[i].remove();
-	document.getElementsByTagName("body")[0].innerHTML="<form> Введите количество строк:"
-		+ '<input type="number" id="row"> Введите количество столбцов:'
-		+ '<input type="number" id="column"><button onclick="createTable()">Submit</button>'
-		+ '<script type="text/javascript" src="tasks2.js"></script></form>';
-}
-
-function createTable(){
-	let row = document.getElementById('row').value;
-	let column = document.getElementById('column').value;
-	let changeWidthText = "Submit";
-
-	document.write("<div> Change width: <input type='text' onInput='changeWidthText()'></input>"
-		+ "<select onChange='changeWidthText()'><option>solid</option><option>dotted</option>"
-		+ "<option>dashed</option><option>double</option></select><input type='button'"
-		+ "onclick='changeWidth()' value=changeWidthText></input></div>");
-	document.write("<div> Add header: <input type='text'></input>"
-		+ "<input type='button' onclick='addHeader()' value=Add header></input></div>");
-	document.write("<div> Delete row: <input type='text'></input>"
-		+ "<input type='button' onclick='deleteRow()' value=Delete row></input></div>");
-	document.write("<div><input type='button' onclick='magic()' value=Magic></input></div>");
-	document.write("<div><input type='button' onclick='deleteThis()' value=Delete></input></div>");
-	
-	for (var i = 0; i < document.getElementsByTagName("div").length; i++) {
-		document.getElementsByTagName("div")[i].style = "float:left";
-	}
-
-	let tbl = document.createElement("table");
-	let tblBody = document.createElement("tbody");
-
-	for (var j = 0; j < row; j++) {
-		let row = document.createElement("tr");
-		for (var i = 0; i < column; i++) {
-			let cell = document.createElement("td");
-			let cellText = document.createElement("input", "type=textarea");
-			let cellText2 = document.createElement("button");
-			cellText2.setAttribute("onClick", "saveText("+j+", "+i+")");
-			cellText2.innerHTML = "Save";
-			cell.appendChild(cellText);
-			cell.appendChild(cellText2);
-			row.appendChild(cell);
-		}
-		tblBody.appendChild(row);
-	}
-
-	tbl.appendChild(tblBody);
-	document.getElementsByTagName('body')[0].appendChild(tbl);
-	tbl.setAttribute('border', '2');
-}
+   function create() {
+            let form = document.getElementById("form");
+            form.style = "display: none";
+            let fun = document.getElementById("Functions");
+            fun.style = "display: block";
+            let rows = +document.getElementById("Rows").value;
+            let cols = +document.getElementById("Cols").value;
+            let tableWrap = document.getElementById("tableWrap");
+            let tableCode = `<table><tbody>`;
+            for (let row = 0; row < rows; ++row) {
+                tableCode += `<tr>`;
+                for (let col = 0; col < cols; ++col) {
+                    tableCode += `<td id="td${row},${col}"><form><input type="textarea" id="f${row},${col}"><input type="button" onclick="changedata(${row}, ${col})" value="Save"></form></td>`;
+                }
+                tableCode += `</tr>`;
+            }
+            tableCode += `</tbody></table>`;
+            tableWrap.innerHTML = tableCode;
+        }
+        function changeBorderButton() {
+            document.querySelector(`#chS`).value = `set ${document.getElementById(`chBS`).value}px ${document.getElementById(`chBT`).value}`;
+        }
+        function setBorder() {
+            let tds = document.querySelectorAll(`#tableWrap td`);
+            tds.forEach(td => {
+                td.style = `border: ${document.getElementById(`chBS`).value}px ${document.getElementById(`chBT`).value}`;
+            });
+        }
+        function changedata(row, col) {
+            let data = document.getElementById(`f${row},${col}`).value;
+            let td = document.getElementById(`td${row},${col}`);
+            td.innerHTML = data;
+        }
+        function addTitle() {
+            let caption = document.querySelectorAll(`#tableWrap caption`).length == 0 ? document.createElement(`caption`) : document.querySelectorAll(`#tableWrap caption`)[0];
+            let table = document.querySelector(`#tableWrap table`);
+            caption.innerHTML = document.querySelector(`#title`).value;
+            table.insertBefore(caption, table.childNodes[0]);
+        }
+        function removeTr() {
+            let trs = document.querySelectorAll(`#tableWrap tr`);
+            if (document.querySelector(`#rmTr`).value >= trs.length) alert("Wrong row number!");
+            else {
+                let tr = trs[document.querySelector(`#rmTr`).value];
+                tr.parentNode.removeChild(tr);
+            }
+        }
+        function makeMagic() {
+            let tds = document.querySelectorAll(`#tableWrap td`);
+            let td = tds[Math.floor(Math.random() * tds.length)];
+            if (!tds.length) return;
+            if (Math.random() >= 0.5) {
+                td.style.backgroundColor = randColor();
+                td.style.color = randColor();
+                td.style.fontSize = `${Math.floor(Math.random() * 10 + 15)}pt`;
+            } else {
+                let coords = td.id.split(`d`)[1];
+                let row = +coords.split(`,`)[0];
+                let col = +coords.split(`,`)[1];
+                td.innerHTML = `<form><input type="textarea" id="f${row},${col}"><input type="button" onclick="changedata(${row}, ${col})" value="Save"></form>`;
+            }
+        }
+        function randColor() {
+            let r = Math.floor(Math.random() * 256);
+            let g = Math.floor(Math.random() * 256);
+            let b = Math.floor(Math.random() * 256);
+            return `rgb(${r},${g},${b})`;
+        }
+        function removeTable() {
+            let form = document.getElementById("form");
+            form.style = "display: block";
+            let fun = document.getElementById("Functions");
+            fun.style = "display: none";
+            let tableWrap = document.getElementById("tableWrap");
+            tableWrap.innerHTML = "";
+        }
